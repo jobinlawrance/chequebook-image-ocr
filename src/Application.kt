bc3fd7cddd86c0f2228ec675ee5bf5d38f10047c
jobinlawrance <jobinlawrance@gmail.com>
2021-02-21 11:46:12 +0530

    Git try heroku deployment

--- src/Application.kt
+++ src/Application.kt
@@ -7,64 +7,32 @@ import io.ktor.routing.*
 import io.ktor.http.*
 import io.ktor.gson.*
 import io.ktor.features.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
 import io.ktor.sessions.*
 
-fun main(args: Array<String>): Unit = io.ktor.server.cio.EngineMain.main(args)
-
-@Suppress("unused") // Referenced in application.conf
-@kotlin.jvm.JvmOverloads
-fun Application.module(testing: Boolean = false) {
-    install(ContentNegotiation) {
-        gson {
-        }
-    }
-
-    install(Sessions) {
-        cookie<MySession>("MY_SESSION") {
-            cookie.extensions["SameSite"] = "lax"
-        }
-    }
-
-    install(CORS) {
-        method(HttpMethod.Options)
-        method(HttpMethod.Put)
-        method(HttpMethod.Delete)
-        method(HttpMethod.Patch)
-        header(HttpHeaders.Authorization)
-        header("MyCustomHeader")
-        allowCredentials = true
-        anyHost() // @TODO: Don't do this in production if possible. Try to limit it.
-    }
-
-    routing {
-        get("/") {
-            call.respondText("HELLO WORLD!", contentType = ContentType.Text.Plain)
-        }
-
-        get("/json/gson") {
-            call.respond(mapOf("hello" to "world"))
-        }
-
-        get("/session/increment") {
-            val session = call.sessions.get<MySession>() ?: MySession()
-            call.sessions.set(session.copy(count = session.count + 1))
-            call.respondText("Counter is ${session.count}. Refresh to increment.")
-        }
-
-        install(StatusPages) {
-            exception<AuthenticationException> { cause ->
-                call.respond(HttpStatusCode.Unauthorized)
+fun main(args: Array<String>) {
+    val port = System.getenv("PORT")?.toInt() ?: 23567
+    embeddedServer(Netty,port) {
+        install(ContentNegotiation) {
+            gson {
+                setPrettyPrinting()
             }
-            exception<AuthorizationException> { cause ->
-                call.respond(HttpStatusCode.Forbidden)
-            }
-
         }
-    }
+        routing {
+            get("") {
+                call.respond("I'm alive!")
+            }
+            get("hello") {
+                call.respond(HttpStatusCode.Accepted, "Hello")
+            }
+            get("random/{min}/{max}") {
+                val min = call.parameters["min"]?.toIntOrNull() ?: 0
+                val max = call.parameters["max"]?.toIntOrNull() ?: 10
+                val randomString = "${(min until max).shuffled().last()}"
+                call.respond(mapOf("value" to randomString))
+            }
+        }
+    }.start(wait = true)
 }
-
-data class MySession(val count: Int = 0)
-
-class AuthenticationException : RuntimeException()
-class AuthorizationException : RuntimeException()
 
